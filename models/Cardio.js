ec3c318ceda98286d1648c7f3ceeeb1781553219
unknown <kevwenner@gmail.com>
2023-02-10 11:18:59 -0500

    added cardio model

---
+++ models/Cardio.js
@@ -0,0 +1,36 @@
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Cardio extends Model{}
+
+Cardio.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        workout: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        distance: {
+            type: DataTypes.INTEGER,
+        },
+        time: {
+            type: DataTypes.TIME,
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'cardio',
+    }
+);
+
+module.exports = Cardio;
