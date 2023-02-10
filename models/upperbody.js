cccdb9911613e5584912b0c381b31b1acaa2a5b1
Melissa Gillen <melissagillen@Melissas-MacBook-Pro.local>
2023-02-10 11:10:27 -0500

    updated model

--- models/upperbody.js
+++ models/upperbody.js
@@ -1,33 +1,39 @@
-const {Model, DataTypes} = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../condfig/connection");
 
-class UpperBody extends Model{}
+class UpperBody extends Model {}
 
 UpperBody.init(
-    {
-    id:{
-       type:DataTypes.INTEGER,
-       primaryKey:true,
-        autoIncrement:true,
-        allowNull:false
-
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
-     workout:{
-        type: DataTypes.STRING,
-        allowNull:false
+    workout: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    description:{
-        type:DataTypes.STRING,
-        allowNull:false
-
-    }
-},
- {
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    numberOfSet: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    numberofRep: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'upperbody',
+    modelName: "upperbody",
   }
-)
+);
 
 module.export = UpperBody;
