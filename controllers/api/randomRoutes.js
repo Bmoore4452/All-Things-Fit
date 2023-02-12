bbb681d6d659c3d666eeff79e58b7c614af1ee46
Bmoore4452 <111284578+Bmoore4452@users.noreply.github.com>
2023-02-12 15:02:18 -0500

    cardio.js file in public

--- controllers/api/randomRoutes.js
+++ controllers/api/randomRoutes.js
@@ -1,4 +1,4 @@
-const { Cardio } = require("../../models");
+const { Cardio, LowerBody, UpperBody } = require("../../models");
 // const { restore, getAttributes } = require('../../models/Cardio');
 
 const router = require("express").Router();
@@ -6,8 +6,14 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
   Cardio.findAll()
     .then((cardioData) => {
+      var workout = {};
+      var cardio = cardioData;
+      var cardioLength = cardio.length;
+
+      workout = Math.floor(Math.random() * cardioLength) + 1;
+
       res.render("random", { workout: cardioData });
-      console.log(cardioData);
+      console.log(workout);
     })
     .catch((err) => {
       console.log(err);
