8762b865e3453456795a4451ef49a15ba82e0f1c
p0ptartz <ericwm420@gmail.com>
2023-02-12 15:39:14 -0500

    populated workouts from DB

--- controllers/api/signUpRoutes.js
+++ controllers/api/signUpRoutes.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
-
+const {User} = require('../../models')
 router.get('/', async(req, res) => {
     res.render("signup")
 });
 
 router.post('/', async(req, res) => {
+    User.create({
+        name: req.body.name,
+        email: req.body.email
+    })
+    .then(createdUser => {
+        req.session.save(()=>{
+            req.session.userId = createdUser.id
+            req.session.loggedIn = true
+            res.redirect("/api/landingpage")
+        })
 
+    })
+    .catch (err => {
+        res.status(500).json(err)
+    })
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,17 +3,27 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const models = require("./models");
+// const path = require("path")
+const session = require("express-session")
+const SequelizeStore = require("connect-session-sequelize")(session.Store)
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(session({
+  secret: "my secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore ({
+    db: sequelize
+  })
+}))
 
 const hbs = exphbs.create({});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
