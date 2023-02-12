7cb6be317608b635ef7fbb2a10c365f8e3d52e6a
melissa.gillen <90412072+StamperM@users.noreply.github.com>
2023-02-11 19:20:32 -0500

    added some route updates

--- controllers/api/instructorRoutes.js
+++ controllers/api/instructorRoutes.js
@@ -6,29 +6,29 @@ const router = require('express').Router();
 // router.get('/instructor', async(req, res)
 
 router.get('/', async (req, res) => {
-    res.render('instructor');
-  });
+  //   res.render('instructor');
+  // });
 
 
-//  try{
-//     const instructorData = await Instructor.findAll({
-//         include:{
-//             attributes:['video','title','type','duration']
-//         }
-//     });
-//     const workouts = instructorData.map((workout) => 
-//         workout.get({plain:true})
-//     );
-//     res.render('instructor',{workouts});
+ try{
+    const instructorData = await Instructor.findAll({
+        include:{
+            attributes:['video','title','type','duration']
+        }
+    });
+    const workouts = instructorData.map((workout) => 
+        workout.get({plain:true})
+    );
+    res.render('instructor',{workouts});
     
-//     if(!instructorData){
-//         res.status(404).json({message:"There are currently no Insturctor Workouts"})
-//     }
-//     res.status(200).json(instructorData);
-//  } catch(err){
-//     res.status(500).json(err);
-//  }
-// });
+    if(!instructorData){
+        res.status(404).json({message:"There are currently no Insturctor Workouts"})
+    }
+    res.status(200).json(instructorData);
+ } catch(err){
+    res.status(500).json(err);
+ }
+});
 
 
 module.exports = router;
