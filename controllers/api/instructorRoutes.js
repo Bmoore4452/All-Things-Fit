1826f97f656d9576d5d033a1bf5e8af737cb6e14
p0ptartz <ericwm420@gmail.com>
2023-02-11 23:27:29 -0500

    api/instructor route functional

--- controllers/api/instructorRoutes.js
+++ controllers/api/instructorRoutes.js
@@ -3,32 +3,16 @@ const { Instructor } = require('../../models');
 
 const router = require('express').Router();
 
-// router.get('/instructor', async(req, res)
-
-router.get('/', async (req, res) => {
-    res.render('instructor');
-  });
-
-
-//  try{
-//     const instructorData = await Instructor.findAll({
-//         include:{
-//             attributes:['video','title','type','duration']
-//         }
-//     });
-//     const workouts = instructorData.map((workout) => 
-//         workout.get({plain:true})
-//     );
-//     res.render('instructor',{workouts});
-    
-//     if(!instructorData){
-//         res.status(404).json({message:"There are currently no Insturctor Workouts"})
-//     }
-//     res.status(200).json(instructorData);
-//  } catch(err){
-//     res.status(500).json(err);
-//  }
-// });
-
+router.get("/", (req, res) => {
+  Instructor.findAll()
+    .then(instructorData => {
+      res.render("instructor", {instructors: instructorData });
+      console.log(instructorData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
