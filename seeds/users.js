65020a2539d497243a90f4812702c74b9c8ba61c
p0ptartz <ericwm420@gmail.com>
2023-02-13 10:38:30 -0500

    added password to User table

--- seeds/users.js
+++ seeds/users.js
@@ -4,18 +4,22 @@ const userdata = [
   {
     name: "Tommy John",
     email: "TommysJohn@gmail.com",
+    password: "JohnTom369"
   },
   {
     name: "Ham Strings",
     email: "HamsStrings@gmail.com",
+    password: "eatMOREkale420"
   },
   {
     name: "Shin Splints",
     email: "ShinsSplint@aol.com",
+    password: "runningHURTSme"
   },
   {
     name: "Dream Shake",
     email: "DreamsShake@hotmail.com",
+    password: "chugMOREmilk"
   },
 ];
 
