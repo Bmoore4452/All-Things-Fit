5c5e739944d5368fcface107907e33eb9595cce8
unknown <kevwenner@gmail.com>
2023-02-13 21:08:05 -0500

    added delete function to workout plan

--- public/js/customWorkout.js
+++ public/js/customWorkout.js
@@ -16,7 +16,28 @@ const addWorkout = () => {
     
     WOprogram.forEach(workout => {
         if(!document.getElementById(workout.title)){
-            WOplan.innerHTML += `<div id="${workout.title}"><h5>${workout.title}</h5><p>${workout.type}</p><p>${workout.desc}</p></div>`;
+            WOplan.innerHTML += `<div id="${workout.title}"><h5>${workout.title}</h5><p>${workout.type}</p><p>${workout.desc}</p><button onclick="kill('${workout.title}')">Delete</button></div>`;
         }
     });
+}
+
+// removes workout from array and view
+const kill = (index) => {
+    //remove array
+    WOprogram.splice(WOprogram.indexOf(index));
+    console.log(WOprogram);
+    //remove html
+    let temp = document.getElementById(index);
+    temp.remove();
+    console.log(`this object should be removed`);
+}
+
+// pushes array to database
+const post = () => {
+    if (WOprogram.length > 0) {
+        console.log(`work out pushed to server`);
+    } else{
+        console.log(`please add a workout to the plan`);
+    }
+    
 }
