9b28a2481aba778d61827c3d79893fae571b8e45
Bmoore4452 <111284578+Bmoore4452@users.noreply.github.com>
2023-02-15 01:51:44 -0500

    updated README

--- server.js
+++ server.js
@@ -3,30 +3,31 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const models = require("./models");
-const path = require("path")
-const session = require("express-session")
-const SequelizeStore = require("connect-session-sequelize")(session.Store)
+const path = require("path");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(session({
-  secret: "my secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore ({
-    db: sequelize
+app.use(
+  session({
+    secret: "my secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
   })
-}))
+);
 
 const hbs = exphbs.create({});
-app.set("views", path.join(__dirname, "./views"));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -34,5 +35,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
